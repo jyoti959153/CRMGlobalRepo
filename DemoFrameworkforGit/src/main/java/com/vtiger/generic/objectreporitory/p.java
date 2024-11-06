e62a65a606efbe4eec90cea5995b8a5dc2611cc6
sanje <sanje@Sanjeev.mshome.net>
2024-11-06 16:23:50 +0530

    Added element1 in p page

--- DemoFrameworkforGit/src/main/java/com/vtiger/generic/objectreporitory/p.java
+++ DemoFrameworkforGit/src/main/java/com/vtiger/generic/objectreporitory/p.java
@@ -1,5 +1,15 @@
 package com.vtiger.generic.objectreporitory;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class p {
+	
+	@FindBy(xpath="//a[text()='Sign']")
+	private WebElement signout;
+
+	public WebElement getSignout() {
+		return signout;
+	}
 
 }
